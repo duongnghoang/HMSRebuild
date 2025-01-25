79f1237ccf94d8529a58e924274615cb57118abb
nguyễn thái thịnh <thaithinhworkspace@gmail.com>
2025-01-25 15:12:29 +0700

    Continue Update Entity

---
+++ src/Domain/DevelopingEntities/RoomEquipment.cs
@@ -0,0 +1,21 @@
+﻿
+using Domain.DomainCommon.Interface;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.DevelopingEntities;
+
+public class RoomEquipment : IResponse<int>
+{
+    [Key]
+    public int Id { get; set; }
+    public int RoomId { get; set; }
+    public int EquipmentId { get; set; }
+    public int Quantity { get; set; }
+    public string Note { get; set; }
+
+    [ForeignKey(nameof(RoomId))]
+    public Room? Room { get; set; }
+    [ForeignKey(nameof(EquipmentId))]
+    public Equipment? Equipment { get; set; }
+}
