cd46a7b78d7b5e951e86ae0cc90236b38b1541d9
nguyễn thái thịnh <thaithinhworkspace@gmail.com>
2025-01-27 12:05:21 +0700

    LogType Udpate

--- src/Domain/Entities/Common/Enumeration/Definition/LogType.cs
+++ src/Domain/Entities/Common/Enumeration/Definition/LogType.cs
@@ -1,5 +1,29 @@
 ﻿namespace Domain.Entities.Common.Enumeration.Definition;
-
-public class LogType
+public enum LogType
 {
+    None,
+    Repair,
+    ChangeCurrency
+}
+public class LogTypeCode
+{
+
+}
+public static class LogTypeConversion
+{
+    public static string LogTypeToString(this LogType logType)
+    {
+        return logType switch
+        {
+           
+        };
+    }
+
+    public static LogType BookingStatusToEnum(string logType)
+    {
+        return logType switch
+        {
+           
+        };
+    }
 }
