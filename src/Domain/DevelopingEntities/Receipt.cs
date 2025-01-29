9d1721e972765a5921b3aad1fb8f0744965a97e3
nguyễn thái thịnh <thaithinhworkspace@gmail.com>
2025-01-30 01:12:19 +0700

    Tạm thời thêm

---
+++ src/Domain/DevelopingEntities/Receipt.cs
@@ -0,0 +1,24 @@
+﻿using Domain.DomainCommon.Interface;
+using Domain.Entities.Common;
+using Domain.Entities.Common.Enumeration.Definition;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.DevelopingEntities;
+
+public class Receipt : TimeStampTracking, IResponse<int>
+{
+    [Key]
+    public int Id { get; set; }
+    public string StaffId { get; set; }
+    public int PaymentMethodId { get; set; }
+    public PaymentStatus PaymentStatus { get; set; } = PaymentStatus.Unpaid;
+    public string GuestName { get; set; }
+
+    [ForeignKey(nameof(StaffId))]
+    public virtual Staff? ReceiptCreatedStaff { get; set; }
+
+    [ForeignKey(nameof(PaymentMethodId))]
+    public virtual PaymentMethod? PaymentMethod { get; set; }
+
+}
---
+++ src/Domain/DevelopingEntities/Transaction.cs
@@ -0,0 +1,12 @@
+﻿using Domain.DomainCommon.Interface;
+using Domain.Entities.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.DevelopingEntities;
+
+public class Transaction : TimeStampTracking, IResponse<int>
+{
+    [Key]
+    public int Id { get; set; }
+    
+}
