26170bd5f27c640d7fc3f138b5b396b178152111
Duong Nguyen Hoang <hoangduongatwork@gmail.com>
2025-02-07 20:06:01 +0700

    fee policy use case

---
+++ src/Application/RoomTypeFees/CreateFeePolicy/CreateFeePolicyCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using Application.Interfaces;
+using Domain.Shared;
+
+namespace Application.RoomTypeFees.CreateFeePolicy
+{
+    public class CreateFeePolicyCommandHandler : ICommandHandler<CreateFeePolicyCommand>
+    {
+        public Task<Result> Handle(CreateFeePolicyCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Domain/Abstractions/Repositories/IFeePolicyRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Abstractions.BaseObjects;
+using Domain.Entities.RoomTypeFees;
+
+namespace Domain.Abstractions.Repositories
+{
+    public interface IFeePolicyRepository : IBaseRepository<FeePolicy>
+    {
+    }
+}
